a09c5e6301b31c13c8dad2590a431260d3fcec57
Leo Xuzhang Lin <me@leoxlin.com>
2025-12-08 23:38:37 -0500

    Test sudo

--- flows/sysadmin.py
+++ flows/sysadmin.py
@@ -1,6 +1,7 @@
 import os
 
 from prefect import flow
+from prefect_shell import ShellOperation
 
 @flow(log_prints=True)
 def sysadmin_backup():
@@ -8,5 +9,12 @@ def sysadmin_backup():
 
 @flow(log_prints=True)
 def snapraid_sync():
-    print(os.getcwd())
-    print("boop")
+    with ShellOperation(
+        commands=[
+            'sudo snapraid --version'
+        ]
+    ) as cmd:
+        proc = cmd.trigger()
+        proc.wait_for_completion()
+        res = proc.fetch_result()
+        print(res)
