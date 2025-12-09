4572920ac45f42b4fbbeca8a13b6775a4f75f10f
Leo Xuzhang Lin <me@leoxlin.com>
2025-12-09 00:01:00 -0500

    Try diff first

--- flows/sysadmin.py
+++ flows/sysadmin.py
@@ -1,20 +1,21 @@
-import os
-
 from prefect import flow
 from prefect_shell import ShellOperation
+from typing import List
+
+def run_shell(cmd: str) -> List[str]:
+    with ShellOperation(
+            commands=[cmd]
+    ) as cmd:
+        proc = cmd.trigger()
+        proc.wait_for_completion()
+        res = proc.fetch_result()
+        return res
 
 @flow(log_prints=True)
 def sysadmin_backup():
     print("boop")
 
 @flow(log_prints=True)
-def snapraid_sync():
-    with ShellOperation(
-        commands=[
-            'sudo snapraid --version'
-        ]
-    ) as cmd:
-        proc = cmd.trigger()
-        proc.wait_for_completion()
-        res = proc.fetch_result()
-        print(res)
+def snapraid_sync(snapraid_conf: str):
+    for line in run_shell(f"sudo snapraid --conf {snapraid_conf} diff"):
+        print(line)
