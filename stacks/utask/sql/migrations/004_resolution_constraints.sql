5cacad14dafdeeb90a885b2209795533a8d76bc4
Leo Xuzhang Lin <me@leoxlin.com>
2025-12-18 20:07:35 -0500

    Prefect still the goat

--- stacks/utask/sql/migrations/004_resolution_constraints.sql
+++
@@ -1,8 +0,0 @@
--- +migrate Up
-
--- Add a missing 'ON DELETE CASCADE' on resolution->task FOREIGN KEY
-ALTER TABLE resolution DROP CONSTRAINT resolution_id_task_fkey, ADD CONSTRAINT resolution_id_task_fkey FOREIGN KEY (id_task) REFERENCES task(id) ON DELETE CASCADE;
-
--- +migrate Down
-
-ALTER TABLE resolution DROP CONSTRAINT resolution_id_task_fkey, ADD CONSTRAINT resolution_id_task_fkey FOREIGN KEY (id_task) REFERENCES task(id);
