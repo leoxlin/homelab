d682a06bec3a3556b6a72a1779271cc016020169
Leo Xuzhang Lin <me@leoxlin.com>
2026-01-15 15:47:05 -0500

    Move flux bootstrap to bootstrap

---
+++ bootstrap/flux.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+
+helm upgrade --install flux-operator oci://ghcr.io/controlplaneio-fluxcd/charts/flux-operator \
+  --namespace flux-system \
+  --create-namespace \
+  --version 0.38.1 \
+  --wait
+
+kubectl apply -f $SCRIPT_DIR/flux-instance.yaml
